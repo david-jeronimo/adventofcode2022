e4d16a73e041e7f9c923665e501a96e9830abdb3
David Jeronimo <david.jeronimo@liberty-it.co.uk>
2023-01-03 08:02:38 +0100

    aoc-20: decrypt (cleanup)

--- src/main/scala/AOC_20.scala
+++ src/main/scala/AOC_20.scala
@@ -28,18 +28,11 @@ object AOC_20 {
     }
 
     def decryptPartTwo(source: Source): BigInt = {
-        val original: List[Item] = source.getLines().map(_.toInt).zipWithIndex.map(e => Item(e._1 * 811589153l, e._2)).toList
-        val result1: List[Item] = original.foldLeft(original)(mix)
-        val result2: List[Item] = original.foldLeft(result1)(mix)
-        val result3: List[Item] = original.foldLeft(result2)(mix)
-        val result4: List[Item] = original.foldLeft(result3)(mix)
-        val result5: List[Item] = original.foldLeft(result4)(mix)
-        val result6: List[Item] = original.foldLeft(result5)(mix)
-        val result7: List[Item] = original.foldLeft(result6)(mix)
-        val result8: List[Item] = original.foldLeft(result7)(mix)
-        val result9: List[Item] = original.foldLeft(result8)(mix)
-        val result: List[Item] = original.foldLeft(result9)(mix)
-        val positionZero = result.indexWhere(i => i.value == 0)
+        val original: List[Item] = source.getLines().map(_.toInt).zipWithIndex.map(e => Item(e._1 * 811589153L, e._2)).toList
+        val result = (0 until 10).foldLeft(original)(
+            (prev, _) => original.foldLeft(prev)(mix)
+        )
+        val positionZero = result.indexWhere(_.value == 0)
         List(
             result((positionZero + 1000) % result.length),
             result((positionZero + 2000) % result.length),
